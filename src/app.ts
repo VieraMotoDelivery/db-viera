2e548dd931757da480177385f891c5efa9e2d109
Viera Moto delivery <vieiramdelivery@gmail.com>
2025-01-10 14:41:40 -0300

    Quantidade de entregas da semana

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use("/fisica", fisicaRoutes);
 app.use("/webhook", webHook);
 app.use(handleErrorMiddleware);
 
-const job = new CronJob("41 14 * * *", async () => {
+const job = new CronJob("43 14 * * *", async () => {
   const entregasRepositorio: Repository<Entregas> =
     AppDataSource.getRepository(Entregas);
 
